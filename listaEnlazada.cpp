e2afcaa8f0ce572a47f3a2b6ec752fc415ea2cf2
matiuuh <estuardonoriega36@gmail.com>
2024-08-03 20:13:57 -0600

    Se agregan las listas enlazadas

---
+++ listaEnlazada.cpp
@@ -0,0 +1,25 @@
+#include "ListaEnlazada.h"
+#include <iostream>
+
+using namespace std;
+
+
+Nodo::Nodo(const Usuario& user, unique_ptr<Nodo> sig)
+    :usuario(user), siguiente(move(sig)) {}
+
+ListaEnlazada::ListaEnlazada() : cabeza(nullptr) {}
+
+void ListaEnlazada::agregarUsuario(const Usuario& usuario) {
+    cabeza = make_unique<Nodo>(usuario, move(cabeza));
+}
+
+void ListaEnlazada::mostrarUsuarios() const {
+    Nodo* actual = cabeza.get();
+    while (actual != nullptr) {
+            cout << "Nombre: " << actual->usuario.getNombre()
+                << ", Apellidos: " << actual->usuario.getApellidos()
+                << ", Fecha de Nacimiento: " << actual->usuario.getFechaNacimiento()
+                << ", Correo: " << actual->usuario.getCorreo() << std::endl;
+        actual = actual->siguiente.get();
+    }
+}
