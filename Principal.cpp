f2a8cf630d0361cb353e2ff9099215aba91a3d61
matiuuh <estuardonoriega36@gmail.com>
2024-08-03 18:30:06 -0600

    Se agrgeea el menu principal

--- Principal.cpp
+++ Principal.cpp
@@ -4,10 +4,51 @@
 
 using namespace std;
 
+//prototipos
+void menu();
 
 int main(){
-    cout<<"hola mundo";
-    cout<<"hola mundo";
+    menu();
     system("pause");
     return 0;
+}
+
+void menu(){
+    int opcion;
+
+    do{
+        cout << "\t-----Menu-----\n";
+        cout << "\t1. Iniciar sesion" << endl;
+        cout << "\t2. Registrarse" << endl;
+        cout << "\t3. Informacion" << endl;
+        cout << "\t4. Salir" << endl;
+        cout << "\tIngrese opcion: "; cin >> opcion;
+
+        switch (opcion) {
+            case 1:
+                cout<<"\n";
+                system("pause");
+                break;
+            case 2:
+                cout<<"\n";
+                system("pause");
+                break;
+            case 3:
+                cout<<"Nombre: Mateo Estuardo Diego Noriega"<<endl;
+                cout<<"Carnet: 202203009"<<endl;
+                cout<<"Link del repositorio: https://github.com/matiuuh/-EDD-Proyecto1_202203009.git";
+                cout<<"\n";
+                system("pause");
+                break;
+            case 4:
+                cout<<"saliendo del programa ";
+                break;
+            default:
+                cout << "Ingrese una opcion valida. ";
+                system("pause");
+                break;
+        }
+        system("cls");
+    }while(opcion!=4);
+    
 }
