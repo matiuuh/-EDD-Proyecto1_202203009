7273f0d94e484c56bdc2c4fec165b31f1441c25e
matiuuh <estuardonoriega36@gmail.com>
2024-10-07 22:10:58 -0600

    Se crea la matriz adyacente (en desarrollo)

---
+++ Fase 2/Fase2EDD/EstructurasAdmin/matrizadyacenteglobal.h
@@ -0,0 +1,38 @@
+#ifndef MATRIZADYACENTEGLOBAL_H
+#define MATRIZADYACENTEGLOBAL_H
+
+#include <vector>
+#include <string>
+
+// Estructura NodoUsuario para la lista enlazada
+struct NodoUsuarioe {
+    std::string correoUsuario;
+    int indice;
+    NodoUsuarioe* siguiente;
+
+    NodoUsuarioe(const std::string& correo, int idx) : correoUsuario(correo), indice(idx), siguiente(nullptr) {}
+};
+
+class MatrizAdyacenteGlobal
+{
+public:
+    MatrizAdyacenteGlobal();
+
+    // Métodos para gestionar la matriz de adyacencia global
+    void agregarUsuario(const std::string& correoUsuario);
+    void agregarRelacion(const std::string& correo1, const std::string& correo2);
+    void eliminarRelacion(const std::string& correo1, const std::string& correo2);
+    bool sonAmigos(const std::string& correo1, const std::string& correo2) const;
+    void graficarMatriz() const;
+
+    static MatrizAdyacenteGlobal& getInstancia();  // Método estático para obtener la instancia única
+
+private:
+    static NodoUsuarioe* listaUsuarios;                    // Lista enlazada de usuarios
+    static std::vector<std::vector<int>> matrizAdyacencia; // Matriz de adyacencia
+    static int numUsuarios;
+
+    int obtenerIndice(const std::string& correo) const;
+};
+
+#endif // MATRIZADYACENTEGLOBAL_H
